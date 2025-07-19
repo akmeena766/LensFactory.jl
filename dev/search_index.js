646de8cedad226c703d3f328c188fd7de6c677d0
Documenter.jl <documenter@juliadocs.github.io>
2025-07-19 07:47:46 +0000

    build based on ab7b7da

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#LensFactory.jl","page":"Index","title":"LensFactory.jl","text":"","category":"section"},{"location":"index.html","page":"Index","title":"Index","text":"Documentation for LensFactory.jl","category":"page"}]
+}
