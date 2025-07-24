f17629ee6443b7a4c1532e7f3a34e8a786432fca
akmeena766 <ashishmeena766@gmail.com>
2025-07-24 11:31:54 +0530

    version 0.0.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,9 @@ lum_dist = Cosmology.luminosity_distance(cosmo, 1)
 
 ang_dist = Cosmology.angular_diameter_distance(cosmo, 0.0, 1.0)
 @test ang_dist/Constants.DIST_MPC ≈ 1651.9144029437339
+
+com_vol_element = Cosmology.comoving_volume_element(cosmo, 1.0)
+@test com_vol_element ≈ 26.550755712253192
+
+com_vol = Cosmology.comoving_volume(cosmo, 1.0)
+@test com_vol ≈ 151.05712532061932
