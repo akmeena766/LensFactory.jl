8b042432e8cd219df19a5493c8eed8a3971c1041
akmeena766 <ashishmeena766@gmail.com>
2025-07-31 22:03:22 +0530

    version 0.0.1

--- src/Lenses/Lenses.jl
+++ src/Lenses/Lenses.jl
@@ -1,7 +1,7 @@
 module Lenses
 
 
-# Inbuilt functions to import
+# Julia inbuilt functions to import
 
 
 # Using cosmology from one level up (i.e., LensFactory.Main)
@@ -13,11 +13,33 @@ using ..Constants
 include("./lens_types.jl")
 
 
-# Gravitational lens modules modules
-
-
 # Various lensing function to export
+export get_meshgrid
 
 
+"""
+     get_meshgrid(θx::RV, θy::RV, pixel_size::RV)
+
+Generate a meshgrid for the x and y dimensions and pixel size.
+"""
+function get_meshgrid(θx::RV, θy::RV, pixel_size::RV)::Tuple{Matrix{<:Float64}, Matrix{<:Float64}}
+   if θx <= 0 || θy <= 0 || pixel_size <= 0
+      throw(ArgumentError("All arguments must be positive."))
+   end
+
+   nx::Int64 = floor(Int64, 2.0*θx/pixel_size + 1 + 0.5)
+   ny::Int64 = floor(Int64, 2.0*θy/pixel_size + 1 + 0.5)
+
+   grid_x = Matrix{Float64}(undef, ny, nx)
+   grid_y = Matrix{Float64}(undef, ny, nx)
+
+   @inbounds for i = 1:nx     # Loop over x-dimension (i.e., rows)
+      @inbounds for j = 1:ny  # Loop over y-dimension (i.e., columns)
+         grid_x[j, i] = - θx + float(i - 1) * pixel_size
+         grid_y[j, i] = - θy + float(j - 1) * pixel_size
+      end
+   end
+   return grid_x, grid_y
 end
 
+end
