becddc3295cdff6c8d849135a8c0b36b4d5a8188
akmeena766 <ashishmeena766@gmail.com>
2025-08-01 15:57:18 +0530

    version 0.0.1

--- src/Lenses/Lenses.jl
+++ src/Lenses/Lenses.jl
@@ -37,21 +37,26 @@ export get_meshgrid
                |        |        |
     (-θx, +θy) +--- dθ --- dθ ---+ (+θx, +θy)
 """
-function get_meshgrid(θx::RV, θy::RV, dθ::RV)::Tuple{Matrix{<:Float64}, Matrix{<:Float64}}
+function get_meshgrid(θx::RV, θy::RV, dθ::RV)#::Tuple{Matrix{<:Float64}, Matrix{<:Float64}}
+   # Making sure that grid and pixel size are positive
    if θx <= 0 || θy <= 0 || dθ <= 0
       throw(ArgumentError("All arguments must be positive."))
    end
 
-   nx::Int64 = floor(Int64, 2.0*θx/dθ + 1 + 0.5)
-   ny::Int64 = floor(Int64, 2.0*θy/dθ + 1 + 0.5)
+   # Number of pixels along x- and y-directions
+   nx::Int64 = floor(Int64, 2.0*θx/dθ + 1.5)
+   ny::Int64 = floor(Int64, 2.0*θy/dθ + 1.5)
 
+   # Initialize an empty nx x ny grid
    grid_x = Matrix{Float64}(undef, ny, nx)
    grid_y = Matrix{Float64}(undef, ny, nx)
 
+   # Filling the empty grid with positions
    @inbounds for i = 1:nx     # Loop over x-dimension (i.e., rows)
+      x_val = - θx + (i - 1.0) * dθ
       @inbounds for j = 1:ny  # Loop over y-dimension (i.e., columns)
-         grid_x[j, i] = - θx + float(i - 1) * dθ
-         grid_y[j, i] = - θy + float(j - 1) * dθ
+         grid_x[j, i] = x_val
+         grid_y[j, i] = - θy + (j - 1.0) * dθ
       end
    end
    return grid_x, grid_y
