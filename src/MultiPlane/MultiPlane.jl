69dcdb4f4e52fa7a5370f902d15e41fd82bb721a
akmeena766 <ashishmeena766@gmail.com>
2025-09-05 15:35:49 +0530

    version 0.0.1+dev

--- src/MultiPlane/MultiPlane.jl
+++ src/MultiPlane/MultiPlane.jl
@@ -54,7 +54,7 @@ function get_deflection(cosmology::Cosmology.AbstractCosmology, lens::Lenses.Abs
 
       # Deflection vector at (θx_i, θy_i) in i-th plane
       ψ_vec[1, ni], ψ_vec[2, ni] = Lenses.get_deflection(lens._plane_[ni], θx_i, θy_i)
-      println(ψ_vec[1, ni]," ", ψ_vec[2, ni])
+
       # Final deflection vector
       distance_ratio = (D_ij[ni+1, n_p+2] / D_ij[1, n_p+2])
       ψr[1] = ψr[1] + distance_ratio * ψ_vec[1, ni]
@@ -195,18 +195,71 @@ function get_jacobian(cosmology::Cosmology.AbstractCosmology, lens::Lenses.Abstr
             dist_ratio = (D_ij[ni+1, n_p+2] / D_ij[1, n_p+2])
             ψrr_tmp .+= dist_ratio .* (U_vec[:, :, ni] * A_vec[:, :, ni])
          end
-         ψxx[i, j], ψyy[i, j], ψxy[i, j], ψyx[i, j] = ψrr[1, 1], ψrr[2, 2], ψrr[1, 2], ψrr[2, 1]
+         ψrr[i, j, 1] = ψrr_tmp[1, 1]
+         ψrr[i, j, 2] = ψrr_tmp[2, 2]
+         ψrr[i, j, 3] = ψrr_tmp[1, 2]
+         ψrr[i, j, 4] = ψrr_tmp[2, 1]
       end
    end
-   return ψxx, ψyy, ψxy, ψyx
+   return ψrr[:,:,1], ψrr[:,:,2], ψrr[:,:,3], ψrr[:,:,4]
 end
 
 
-function get_time_delay(cosmology::Cosmology.AbstractCosmology, lens::Lenses.AbstractLens, θx::T, θy::T, zs::RV, β::NTuple{2, RV}) where T <: Union{RV, ROA}
-   # If RV is passed, covert to vector
-   θx = isa(θx, RV) ? [θx] : θx
-   θy = isa(θy, RV) ? [θy] : θy
+function get_time_delay(cosmology::Cosmology.AbstractCosmology, lens::Lenses.AbstractLens, θx::T, θy::T, zs::RV, β::NTuple{2, RV}) where T <: RV
+   # Initialize zero-valued time delay function
+   ϕ = 0.0
 
+   # Get the number of lens planes
+   n_p = lens.n_p
+
+   # Get distance matrix
+   D_ij = _distances(cosmology, lens, zs)
+
+   # Temporary deflection vector for each lens plane
+   ψ_vec = zeros(2, n_p)
+   ψ = 0.0
+
+   # Get constant factor array
+   constant_factor = zeros(n_p)
+   for ni in 1:n_p
+      constant_factor[ni] = ( (1.0 + lens.z_d[ni]) / CONST_C ) * (D_ij[1, ni+1] * D_ij[1, ni+2] / D_ij[ni+1, ni+2])
+   end
+
+   # Loop over all coordinates
+   for ni in 1:n_p
+      # Position vector in 1-st plane
+      θx_i = θx
+      θy_i = θy
+
+      # Get the position vector in i-th plane
+      for nj in 1:ni-1
+         distance_ratio = (D_ij[nj+1, ni+1] / D_ij[1, ni+1])
+         θx_i = θx_i - distance_ratio * ψ_vec[1, nj]
+         θy_i = θy_i - distance_ratio * ψ_vec[2, nj]            
+      end   
+
+      # Potential value at (θ_xi, θ_yi) in i-th plane
+      ψ = Lenses.get_potential(lens._plane_[ni], θx_i, θy_i)
+
+      # Deflection vector at (θx_i, θy_i) in i-th plane
+      ψ_vec[1, ni], ψ_vec[2, ni] = Lenses.get_deflection(lens._plane_[ni], θx_i, θy_i)
+
+      # Get the position vector in i+1-th plane
+      if ni < n_p
+         distance_ratio = (D_ij[ni+1, ni+2] / D_ij[1, ni+2])
+         θx_j = θx_i -  distance_ratio * ψ_vec[1, ni]
+         θy_j = θy_i -  distance_ratio * ψ_vec[2, ni]
+      else
+         θx_j = β[1]
+         θy_j = β[2]
+      end
+      # Time delay factor (without a reference point)
+      ϕ += constant_factor[ni] * (0.5 * ((θx_i-θx_j)^2 + (θy_i-θy_j)^2) - (D_ij[ni+1, ni+2] / D_ij[1, ni+2]) * ψ[1])
+   end
+   return ϕ
+end
+
+function get_time_delay(cosmology::Cosmology.AbstractCosmology, lens::Lenses.AbstractLens, θx::T, θy::T, zs::RV, β::NTuple{2, RV}) where T <: ROA
    # Initialize zero-valued time delay function
    ϕ = zero(θx)
 
@@ -218,7 +271,6 @@ function get_time_delay(cosmology::Cosmology.AbstractCosmology, lens::Lenses.Abs
 
    # Temporary deflection vector for each lens plane
    ψ_vec = zeros(2, n_p)
-   θx_i, θy_i = 0.0, 0.0
    ψ = 0.0
 
    # Get constant factor array
