6ba49e67795106afcaf84e7224154a1e1f7e962a
akmeena766 <ashishmeena766@gmail.com>
2025-10-13 17:21:23 +0530

    version 0.0.3+dev

--- test/test_Lenses_ExternalEffects.jl
+++ test/test_Lenses_ExternalEffects.jl
@@ -12,7 +12,36 @@
    @test jac1[2] ≈ (kappa - gamma1) atol=1e-15 rtol=1e-15
    @test jac1[3] ≈ gamma2 atol=1e-15 rtol=1e-15
 
-   kappa, gamma1, gamma2 = 0.5, 0.0, 0.1
+   pot2 = Lenses.get_potential(lens, xt2, yt2)
+   dex2 = Lenses.get_deflection(lens, xt2, yt2)
+   jac2 = Lenses.get_jacobian(lens, xt2, yt2)
+   @test pot2 ≈ 0.5 * (kappa + gamma1) * xt2^2 + 0.5 * (kappa - gamma1) * yt2^2 + gamma2 * xt2 * yt2 atol=1e-15 rtol=1e-15
+   @test dex2[1] ≈ (kappa + gamma1) * xt2 + gamma2 * yt2 atol=1e-15 rtol=1e-15
+   @test dex2[2] ≈ (kappa - gamma1) * yt2 + gamma2 * xt2 atol=1e-15 rtol=1e-15
+   @test jac2[1] ≈ (kappa + gamma1) atol=1e-15 rtol=1e-15
+   @test jac2[2] ≈ (kappa - gamma1) atol=1e-15 rtol=1e-15
+   @test jac2[3] ≈ gamma2 atol=1e-15 rtol=1e-15
+
+   potc = Lenses.get_potential(lens, [xt1, xt2], [yt1, yt2])
+   dexc = Lenses.get_deflection(lens, [xt1, xt2], [yt1, yt2])
+   jacc = Lenses.get_jacobian(lens, [xt1, xt2], [yt1, yt2])
+   @test potc[1] ≈ pot1 atol=1e-15 rtol=1e-15
+   @test potc[2] ≈ pot2 atol=1e-15 rtol=1e-15
+   
+   @test dexc[1][1] ≈ dex1[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][1] ≈ dex1[2] atol=1e-15 rtol=1e-15
+   @test dexc[1][2] ≈ dex2[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][2] ≈ dex2[2] atol=1e-15 rtol=1e-15
+
+   @test jacc[1][1] ≈ jac1[1] atol=1e-15 rtol=1e-15
+   @test jacc[1][2] ≈ jac2[1] atol=1e-15 rtol=1e-15
+   @test jacc[2][1] ≈ jac1[2] atol=1e-15 rtol=1e-15
+   @test jacc[2][2] ≈ jac2[2] atol=1e-15 rtol=1e-15
+   @test jacc[3][1] ≈ jac1[3] atol=1e-15 rtol=1e-15
+   @test jacc[3][2] ≈ jac2[3] atol=1e-15 rtol=1e-15
+
+   
+   kappa, gamma1, gamma2 = 0.5, 0.1, 0.1
    lens = Lenses.init_ExternalEffects(kappa=kappa, gamma1=gamma1, gamma2=gamma2)
    pot1 = Lenses.get_potential(lens, xt1, yt1)
    dex1 = Lenses.get_deflection(lens, xt1, yt1)
@@ -24,7 +53,35 @@
    @test jac1[2] ≈ (kappa - gamma1) atol=1e-15 rtol=1e-15
    @test jac1[3] ≈ gamma2 atol=1e-15 rtol=1e-15
 
-   kappa, gamma1, gamma2 = 0.5, 0.2, 0.0
+   pot2 = Lenses.get_potential(lens, xt2, yt2)
+   dex2 = Lenses.get_deflection(lens, xt2, yt2)
+   jac2 = Lenses.get_jacobian(lens, xt2, yt2)
+   @test pot2 ≈ 0.5 * (kappa + gamma1) * xt2^2 + 0.5 * (kappa - gamma1) * yt2^2 + gamma2 * xt2 * yt2 atol=1e-15 rtol=1e-15
+   @test dex2[1] ≈ (kappa + gamma1) * xt2 + gamma2 * yt2 atol=1e-15 rtol=1e-15
+   @test dex2[2] ≈ (kappa - gamma1) * yt2 + gamma2 * xt2 atol=1e-15 rtol=1e-15
+   @test jac2[1] ≈ (kappa + gamma1) atol=1e-15 rtol=1e-15
+   @test jac2[2] ≈ (kappa - gamma1) atol=1e-15 rtol=1e-15
+   @test jac2[3] ≈ gamma2 atol=1e-15 rtol=1e-15
+
+   potc = Lenses.get_potential(lens, [xt1, xt2], [yt1, yt2])
+   dexc = Lenses.get_deflection(lens, [xt1, xt2], [yt1, yt2])
+   jacc = Lenses.get_jacobian(lens, [xt1, xt2], [yt1, yt2])
+   @test potc[1] ≈ pot1 atol=1e-15 rtol=1e-15
+   @test potc[2] ≈ pot2 atol=1e-15 rtol=1e-15
+   
+   @test dexc[1][1] ≈ dex1[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][1] ≈ dex1[2] atol=1e-15 rtol=1e-15
+   @test dexc[1][2] ≈ dex2[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][2] ≈ dex2[2] atol=1e-15 rtol=1e-15
+
+   @test jacc[1][1] ≈ jac1[1] atol=1e-15 rtol=1e-15
+   @test jacc[1][2] ≈ jac2[1] atol=1e-15 rtol=1e-15
+   @test jacc[2][1] ≈ jac1[2] atol=1e-15 rtol=1e-15
+   @test jacc[2][2] ≈ jac2[2] atol=1e-15 rtol=1e-15
+   @test jacc[3][1] ≈ jac1[3] atol=1e-15 rtol=1e-15
+   @test jacc[3][2] ≈ jac2[3] atol=1e-15 rtol=1e-15
+
+   kappa, gamma1, gamma2 = 0.3, 0.0, 0.1
    lens = Lenses.init_ExternalEffects(kappa=kappa, gamma1=gamma1, gamma2=gamma2)
    pot1 = Lenses.get_potential(lens, xt1, yt1)
    dex1 = Lenses.get_deflection(lens, xt1, yt1)
@@ -36,39 +93,72 @@
    @test jac1[2] ≈ (kappa - gamma1) atol=1e-15 rtol=1e-15
    @test jac1[3] ≈ gamma2 atol=1e-15 rtol=1e-15
 
-   kappa, gamma1, gamma2 = 0, 0.1, 0.1
+   pot2 = Lenses.get_potential(lens, xt2, yt2)
+   dex2 = Lenses.get_deflection(lens, xt2, yt2)
+   jac2 = Lenses.get_jacobian(lens, xt2, yt2)
+   @test pot2 ≈ 0.5 * (kappa + gamma1) * xt2^2 + 0.5 * (kappa - gamma1) * yt2^2 + gamma2 * xt2 * yt2 atol=1e-15 rtol=1e-15
+   @test dex2[1] ≈ (kappa + gamma1) * xt2 + gamma2 * yt2 atol=1e-15 rtol=1e-15
+   @test dex2[2] ≈ (kappa - gamma1) * yt2 + gamma2 * xt2 atol=1e-15 rtol=1e-15
+   @test jac2[1] ≈ (kappa + gamma1) atol=1e-15 rtol=1e-15
+   @test jac2[2] ≈ (kappa - gamma1) atol=1e-15 rtol=1e-15
+   @test jac2[3] ≈ gamma2 atol=1e-15 rtol=1e-15
+
+   potc = Lenses.get_potential(lens, [xt1, xt2], [yt1, yt2])
+   dexc = Lenses.get_deflection(lens, [xt1, xt2], [yt1, yt2])
+   jacc = Lenses.get_jacobian(lens, [xt1, xt2], [yt1, yt2])
+   @test potc[1] ≈ pot1 atol=1e-15 rtol=1e-15
+   @test potc[2] ≈ pot2 atol=1e-15 rtol=1e-15
+   
+   @test dexc[1][1] ≈ dex1[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][1] ≈ dex1[2] atol=1e-15 rtol=1e-15
+   @test dexc[1][2] ≈ dex2[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][2] ≈ dex2[2] atol=1e-15 rtol=1e-15
+
+   @test jacc[1][1] ≈ jac1[1] atol=1e-15 rtol=1e-15
+   @test jacc[1][2] ≈ jac2[1] atol=1e-15 rtol=1e-15
+   @test jacc[2][1] ≈ jac1[2] atol=1e-15 rtol=1e-15
+   @test jacc[2][2] ≈ jac2[2] atol=1e-15 rtol=1e-15
+   @test jacc[3][1] ≈ jac1[3] atol=1e-15 rtol=1e-15
+   @test jacc[3][2] ≈ jac2[3] atol=1e-15 rtol=1e-15
+   
+   
+   kappa, gamma1, gamma2 = 0.5, 0.1, 0.0
    lens = Lenses.init_ExternalEffects(kappa=kappa, gamma1=gamma1, gamma2=gamma2)
-   pot1 = Lenses.get_potential(lens, xt2, yt2)
-   dex1 = Lenses.get_deflection(lens, xt2, yt2)
-   jac1 = Lenses.get_jacobian(lens, xt2, yt2)
-   @test pot1 ≈ 0.5 * (kappa + gamma1) * xt2^2 + 0.5 * (kappa - gamma1) * yt2^2 + gamma2 * xt2 * yt2 atol=1e-15 rtol=1e-15
-   @test dex1[1] ≈ (kappa + gamma1) * xt2 + gamma2 * yt2 atol=1e-15 rtol=1e-15
-   @test dex1[2] ≈ (kappa - gamma1) * yt2 + gamma2 * xt2 atol=1e-15 rtol=1e-15
+   pot1 = Lenses.get_potential(lens, xt1, yt1)
+   dex1 = Lenses.get_deflection(lens, xt1, yt1)
+   jac1 = Lenses.get_jacobian(lens, xt1, yt1)
+   @test pot1 ≈ 0.5 * (kappa + gamma1) * xt1^2 + 0.5 * (kappa - gamma1) * yt1^2 + gamma2 * xt1 * yt1 atol=1e-15 rtol=1e-15
+   @test dex1[1] ≈ (kappa + gamma1) * xt1 + gamma2 * yt1 atol=1e-15 rtol=1e-15
+   @test dex1[2] ≈ (kappa - gamma1) * yt1 + gamma2 * xt1 atol=1e-15 rtol=1e-15
    @test jac1[1] ≈ (kappa + gamma1) atol=1e-15 rtol=1e-15
    @test jac1[2] ≈ (kappa - gamma1) atol=1e-15 rtol=1e-15
    @test jac1[3] ≈ gamma2 atol=1e-15 rtol=1e-15
 
-   kappa, gamma1, gamma2 = 0.5, 0.0, 0.1
-   lens = Lenses.init_ExternalEffects(kappa=kappa, gamma1=gamma1, gamma2=gamma2)
-   pot1 = Lenses.get_potential(lens, xt2, yt2)
-   dex1 = Lenses.get_deflection(lens, xt2, yt2)
-   jac1 = Lenses.get_jacobian(lens, xt2, yt2)
-   @test pot1 ≈ 0.5 * (kappa + gamma1) * xt2^2 + 0.5 * (kappa - gamma1) * yt2^2 + gamma2 * xt2 * yt2 atol=1e-15 rtol=1e-15
-   @test dex1[1] ≈ (kappa + gamma1) * xt2 + gamma2 * yt2 atol=1e-15 rtol=1e-15
-   @test dex1[2] ≈ (kappa - gamma1) * yt2 + gamma2 * xt2 atol=1e-15 rtol=1e-15
-   @test jac1[1] ≈ (kappa + gamma1) atol=1e-15 rtol=1e-15
-   @test jac1[2] ≈ (kappa - gamma1) atol=1e-15 rtol=1e-15
-   @test jac1[3] ≈ gamma2 atol=1e-15 rtol=1e-15
+   pot2 = Lenses.get_potential(lens, xt2, yt2)
+   dex2 = Lenses.get_deflection(lens, xt2, yt2)
+   jac2 = Lenses.get_jacobian(lens, xt2, yt2)
+   @test pot2 ≈ 0.5 * (kappa + gamma1) * xt2^2 + 0.5 * (kappa - gamma1) * yt2^2 + gamma2 * xt2 * yt2 atol=1e-15 rtol=1e-15
+   @test dex2[1] ≈ (kappa + gamma1) * xt2 + gamma2 * yt2 atol=1e-15 rtol=1e-15
+   @test dex2[2] ≈ (kappa - gamma1) * yt2 + gamma2 * xt2 atol=1e-15 rtol=1e-15
+   @test jac2[1] ≈ (kappa + gamma1) atol=1e-15 rtol=1e-15
+   @test jac2[2] ≈ (kappa - gamma1) atol=1e-15 rtol=1e-15
+   @test jac2[3] ≈ gamma2 atol=1e-15 rtol=1e-15
 
-   kappa, gamma1, gamma2 = 0.5, 0.2, 0.0
-   lens = Lenses.init_ExternalEffects(kappa=kappa, gamma1=gamma1, gamma2=gamma2)
-   pot1 = Lenses.get_potential(lens, xt2, yt2)
-   dex1 = Lenses.get_deflection(lens, xt2, yt2)
-   jac1 = Lenses.get_jacobian(lens, xt2, yt2)
-   @test pot1 ≈ 0.5 * (kappa + gamma1) * xt2^2 + 0.5 * (kappa - gamma1) * yt2^2 + gamma2 * xt2 * yt2 atol=1e-15 rtol=1e-15
-   @test dex1[1] ≈ (kappa + gamma1) * xt2 + gamma2 * yt2 atol=1e-15 rtol=1e-15
-   @test dex1[2] ≈ (kappa - gamma1) * yt2 + gamma2 * xt2 atol=1e-15 rtol=1e-15
-   @test jac1[1] ≈ (kappa + gamma1) atol=1e-15 rtol=1e-15
-   @test jac1[2] ≈ (kappa - gamma1) atol=1e-15 rtol=1e-15
-   @test jac1[3] ≈ gamma2 atol=1e-15 rtol=1e-15
+   potc = Lenses.get_potential(lens, [xt1, xt2], [yt1, yt2])
+   dexc = Lenses.get_deflection(lens, [xt1, xt2], [yt1, yt2])
+   jacc = Lenses.get_jacobian(lens, [xt1, xt2], [yt1, yt2])
+   @test potc[1] ≈ pot1 atol=1e-15 rtol=1e-15
+   @test potc[2] ≈ pot2 atol=1e-15 rtol=1e-15
+   
+   @test dexc[1][1] ≈ dex1[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][1] ≈ dex1[2] atol=1e-15 rtol=1e-15
+   @test dexc[1][2] ≈ dex2[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][2] ≈ dex2[2] atol=1e-15 rtol=1e-15
+
+   @test jacc[1][1] ≈ jac1[1] atol=1e-15 rtol=1e-15
+   @test jacc[1][2] ≈ jac2[1] atol=1e-15 rtol=1e-15
+   @test jacc[2][1] ≈ jac1[2] atol=1e-15 rtol=1e-15
+   @test jacc[2][2] ≈ jac2[2] atol=1e-15 rtol=1e-15
+   @test jacc[3][1] ≈ jac1[3] atol=1e-15 rtol=1e-15
+   @test jacc[3][2] ≈ jac2[3] atol=1e-15 rtol=1e-15
 end
