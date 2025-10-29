58141e042908ed2c40612842a92480f3bf9e5bec
akmeena766 <ashishmeena766@gmail.com>
2025-10-29 20:56:09 +0530

    version 0.0.3+dev

--- src/Lenses/EinastoLens.jl
+++ src/Lenses/EinastoLens.jl
@@ -14,17 +14,21 @@ export jacobian!
 
 
 function m_Ein(θ::RV, n::RV)
-   Pax, _ = gamma_inc(3.0 / n, (2.0 / n) * θ)
+   Pax, _ = gamma_inc(3.0 / n, (2.0 / n) * θ^n)
    return (1.0 / n) * (n / 2.0)^(3.0 / n) * gamma(3.0 / n) * Pax
 end
 
-function ϕ(θ::RV, n::RV)
-   i_value, _ = quadgk(x -> α(θ, n), 0, θ)
-   return i_value
+@inline function I_κ(z::RV, θ::RV, n::RV)
+   return exp(-(2.0 / n) * (θ^2 + z^2)^(0.5 * n))
 end
 
-function I_α(z::RV, θ::RV, n::RV)
-   return ( m_Ein(θ*sqrt(1.0+z^2), n) + θ * m_Ein(θ*sqrt(1.0+z^2)/z, n) ) / (1.0+z^2)^1.5
+function κ(θ::RV, n::RV)
+   i_value, _  = quadgk(x -> I_κ(x, θ, n), 0, Inf)
+   return 0.5 * i_value
+end
+
+@inline function I_α(z::RV, θ::RV, n::RV)
+   return (m_Ein(θ * sqrt(1.0 + z^2), n) + θ * m_Ein(θ * sqrt(1.0 + z^2) / z, n)) / (1.0 + z)^1.5
 end
 
 function α(θ::RV, n::RV)
@@ -32,13 +36,9 @@ function α(θ::RV, n::RV)
    return i_value / θ
 end
 
-function I_κ(z::RV, θ::RV, n::RV)
-   return exp(-(2.0 / n) * (θ^2 + z^2)^(0.5 * n))
-end
-
-function κ(θ::RV, n::RV)
-   i_value, _  = quadgk(x -> I_κ(x, θ, n), 0, Inf)
-   return 0.5 * i_value
+function ϕ(θ::RV, n::RV)
+   i_value, _ = quadgk(x -> α(θ, n), 0, θ)
+   return i_value
 end
 
 function potential!(ψ::T, θx::T, θy::T, D_d::RV, θxc::RV, θyc::RV, ρs:: RV, θs::RV, n::RV) where T <: RV
@@ -101,14 +101,17 @@ function jacobian!(ψxx::T, ψyy::T, ψxy::T, θx::T, θy::T, D_d::RV, θxc::RV,
    dx = (θx - θxc) / θs
    dy = (θy - θyc) / θs
    dr = sqrt(dx^2 + dy^2)
-   
-   ψxx_up = ψxx + κs * ((2.0 * κ(dr, n) * (dx^2 / dr^2)) - (α(dr, n) * (dx^2 - dy^2) / dr^3))
-   ψyy_up = ψyy + κs * ((2.0 * κ(dr, n) * (dy^2 / dr^2)) + (α(dr, n) * (dx^2 - dy^2) / dr^3))
-   ψxy_up = ψxy + κs * (2.0 * dx * dy * (κ(dr, n) - (α(dr, n) / dr)) / dr^2)
+
+   α_r = κs * α(dr, n)
+   κ_r = κs * κ(dr, n)
+
+   ψxx_up = ψxx + 2.0 * κ_r * dx^2 / dr^2 - α_r * (dx^2 - dy^2) / dr^3
+   ψyy_up = ψyy + 2.0 * κ_r * dy^2 / dr^2 + α_r * (dx^2 - dy^2) / dr^3
+   ψxy_up = ψxy + 2.0 * (κ_r - α_r / dr) * dx * dy / dr^2
    return ψxx_up, ψyy_up, ψxy_up
 end
 
-function deflection!(ψxx::T, ψyy::T, ψxy::T, θx::T, θy::T, D_d::RV, θxc::RV, θyc::RV, ρs:: RV, θs::RV, n::RV) where T <: ROA
+function jacobian!(ψxx::T, ψyy::T, ψxy::T, θx::T, θy::T, D_d::RV, θxc::RV, θyc::RV, ρs:: RV, θs::RV, n::RV) where T <: ROA
    κs = 4.0 * ρs * D_d * θs * ANGLE_ARCSEC / (CONST_C^2 / 4.0 / pi / CONST_G / D_d)
 
    ax1, ax2 = axes(θx, 1), axes(θx, 2)
@@ -118,9 +121,12 @@ function deflection!(ψxx::T, ψyy::T, ψxy::T, θx::T, θy::T, D_d::RV, θxc::R
          dy = (θy[i, j] - θyc) / θs
          dr = sqrt(dx^2 + dy^2)
 
-         ψxx[i, j] = ψxx[i, j] + κs * ((2.0 * κ(dr, n) * (dx^2 / dr^2)) - (α(dr, n) * (dx^2 - dy^2) / dr^3))
-         ψyy[i, j] = ψyy[i, j] + κs * ((2.0 * κ(dr, n) * (dy^2 / dr^2)) + (α(dr, n) * (dx^2 - dy^2) / dr^3))
-         ψxy[i, j] = ψxy[i, j] + κs * (2.0 * dx * dy * (κ(dr, n) - (α(dr, n) / dr)) / dr^2)
+         α_r = κs * α(dr, n)
+         κ_r = κs * κ(dr, n)
+
+         ψxx[i, j] = ψxx[i, j] + 2.0 * κ_r * dx^2 / dr^2 - α_r * (dx^2 - dy^2) / dr^3
+         ψyy[i, j] = ψyy[i, j] + 2.0 * κ_r * dy^2 / dr^2 + α_r * (dx^2 - dy^2) / dr^3
+         ψxy[i, j] = ψxy[i, j] + 2.0 * (κ_r - α_r / dr) * dx * dy / dr^2
       end
    end
 end
