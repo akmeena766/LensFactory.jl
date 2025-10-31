3d2f465a6c49dcc9ee09711a2db4f7cd7646b26d
akmeena766 <ashishmeena766@gmail.com>
2025-10-31 14:38:54 +0530

    version 0.0.3+dev

---
+++ test/test_Lenses_gNFWLens.jl
@@ -0,0 +1,44 @@
+#!!!!!!!!!!!!!! Testing/cross-checked AGAINST Glafic !!!!!!!!!!!!!!
+#! Currently I am removing potential tests for gNFW lens model as it makes the compilation very slow.
+@testset "gNFW lens" begin
+   mass = 1E11 * MASS_SUN
+   lens = Lenses.init_gNFWLens(cosmo, zl; mass=mass, c=6, n=0.5)
+
+   dex1 = adis .* Lenses.get_deflection(lens, xt1, yt1)
+   jac1 = adis .* Lenses.get_jacobian(lens, xt1, yt1)
+   kappa = 0.5 * (jac1[1] + jac1[2])
+   gamma1 = 0.5 * (jac1[1] - jac1[2])
+   gamma2 = jac1[3]
+   @test dex1[1] ≈ 0.023681 atol=1e-4 rtol=1e-4
+   @test dex1[2] ≈ 0.023681 atol=1e-4 rtol=1e-4
+   @test kappa  ≈ +0.016830 atol=1e-4 rtol=1e-4
+   @test gamma1 ≈ +0.000000 atol=1e-4 rtol=1e-4
+   @test gamma2 ≈ -0.006850 atol=1e-4 rtol=1e-4
+
+   dex2 = adis .* Lenses.get_deflection(lens, xt2, yt2)
+   jac2 = adis .* Lenses.get_jacobian(lens, xt2, yt2)
+   kappa = 0.5 * (jac2[1] + jac2[2])
+   gamma1 = 0.5 * (jac2[1] - jac2[2])
+   gamma2 = jac2[3]
+   @test dex2[1] ≈ 0.028384 atol=1e-4 rtol=1e-4
+   @test dex2[2] ≈ 0.000000 atol=1e-4 rtol=1e-4
+   @test kappa  ≈ +0.021728 atol=1e-4 rtol=1e-4
+   @test gamma1 ≈ -0.006655 atol=1e-4 rtol=1e-4
+   @test gamma2 ≈ -0.000000 atol=1e-4 rtol=1e-4
+   
+   dexc = adis .* Lenses.get_deflection(lens, [xt1, xt2], [yt1, yt2])
+   jacc = adis .* Lenses.get_jacobian(lens, [xt1, xt2], [yt1, yt2])
+   @test dexc[1][1] ≈ dex1[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][1] ≈ dex1[2] atol=1e-15 rtol=1e-15
+   @test dexc[1][2] ≈ dex2[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][2] ≈ dex2[2] atol=1e-15 rtol=1e-15
+
+   @test jacc[1][1] ≈ jac1[1] atol=1e-15 rtol=1e-15
+   @test jacc[1][2] ≈ jac2[1] atol=1e-15 rtol=1e-15
+   @test jacc[2][1] ≈ jac1[2] atol=1e-15 rtol=1e-15
+   @test jacc[2][2] ≈ jac2[2] atol=1e-15 rtol=1e-15
+   @test jacc[3][1] ≈ jac1[3] atol=1e-15 rtol=1e-15
+   @test jacc[3][2] ≈ jac2[3] atol=1e-15 rtol=1e-15
+
+   @test_throws ArgumentError Lenses.init_EinastoLens(cosmo, zl, mass=mass)
+end
