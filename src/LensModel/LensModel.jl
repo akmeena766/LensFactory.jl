f5e0e617b2941a164c0ce97561a2afa4dd4a59a2
akmeena766 <ashishmeena766@gmail.com>
2026-01-17 10:02:43 +0530

    version 0.0.5+dev

--- src/LensModel/LensModel.jl
+++ src/LensModel/LensModel.jl
@@ -11,7 +11,6 @@ using ..LFUtils
 # Include the lens model IO file
 include("./LensModelIO.jl")
 
-export read_input
-
+export read_input, fit_model
 
 end
---
+++ src/LensModel/LensModelFit.jl
@@ -0,0 +1,9 @@
+
+
+function _generate_model(model_config::ModelConfig)
+   
+end
+
+function fit_model(model_config::ModelConfig)
+   
+end
