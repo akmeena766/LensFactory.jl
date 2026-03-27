4d558b66663975641a0d1ba4e7f4b2e8d0111c2f
akmeena766 <ashishmeena766@gmail.com>
2026-03-27 14:57:41 +0530

    version 0.0.6+dev

--- src/Lenses/ExternalEffects3.jl
+++ src/Lenses/ExternalEffects3.jl
@@ -92,11 +92,15 @@ function deflection!(ψx::T, ψy::T, θx::T, θy::T, δ::RV, ϕ::RV) where T <: 
    cos_value = (θx * cos_phi + θy * sin_phi) / r
    sin_value = (θy * cos_phi - θx * sin_phi) / r
 
+   # cos3(θ - ϕ) and sin3(θ - ϕ)
+   cos_3value = 4.0 * cos_value^3 - 3.0 * cos_value
+   sin_3value = 3.0 * sin_value - 4.0 * sin_value^3
+
    # Deflection
-   term1 = 3.0 * cos_value * sin_value^2
-   term2 = sin_value * (2.0 * cos_value^2 - sin_value^2)
-   ψx_up = ψx + δ * r * (term1 * θx - term2 * θy)
-   ψy_up = ψy + δ * r * (term1 * θy + term2 * θx)
+   term1 = 3.0 * (cos_value - cos_3value)
+   term2 = (sin_value - 3.0 * sin_3value)
+   ψx_up = ψx + 0.25 * δ * r * (term1 * θx + term2 * θy)
+   ψy_up = ψy + 0.25 * δ * r * (term1 * θy - term2 * θx)
    return ψx_up, ψy_up
 end
 
@@ -132,11 +136,15 @@ function deflection!(ψx::T, ψy::T, θx::T, θy::T, δ::RV, ϕ::RV) where T <: 
          cos_value = (dx * cos_phi + dy * sin_phi) / r
          sin_value = (dy * cos_phi - dx * sin_phi) / r
 
+         # cos3(θ - ϕ) and sin3(θ - ϕ)
+         cos_3value = 4.0 * cos_value^3 - 3.0 * cos_value
+         sin_3value = 3.0 * sin_value - 4.0 * sin_value^3
+
          # Deflection
-         term1 = 3.0 * cos_value * sin_value^2
-         term2 = sin_value * (2.0 * cos_value^2 - sin_value^2)
-         ψx[i, j] = ψx[i, j] + δ * r * (term1 * dx - term2 * dy)
-         ψy[i, j] = ψy[i, j] + δ * r * (term1 * dy + term2 * dx)
+         term1 = 3.0 * (cos_value - cos_3value)
+         term2 = (sin_value - 3.0 * sin_3value)
+         ψx[i, j] = ψx[i, j] + 0.25 * δ * r * (term1 * dx + term2 * dy)
+         ψy[i, j] = ψy[i, j] + 0.25 * δ * r * (term1 * dy - term2 * dx)
       end
    end
 end
@@ -146,15 +154,76 @@ end
     jacobian!(ψxx::T, ψyy::T, ψxy::T, θx::T, θy::T, δ::RV, ϕ::RV) where T <: RV
 """
 function jacobian!(ψxx::T, ψyy::T, ψxy::T, θx::T, θy::T, δ::RV, ϕ::RV) where T <: RV
+   # Precompute angles
+   ϕ = deg2rad(ϕ)
+   cos_phi = cos(ϕ)
+   sin_phi = sin(ϕ)
+   
+   # Get radial coordinate
+   r = sqrt(θx^2 + θy^2)
 
+   # cos(θ - ϕ) and sin(θ - ϕ)
+   cos_value = (θx * cos_phi + θy * sin_phi) / r
+   sin_value = (θy * cos_phi - θx * sin_phi) / r
+
+   # cos3(θ - ϕ) and sin3(θ - ϕ)
+   cos_3value = 4.0 * cos_value^3 - 3.0 * cos_value
+   sin_3value = 3.0 * sin_value - 4.0 * sin_value^3
+
+   # Jacobian
+   term1 = 3.0 * (cos_value - cos_3value)
+   term2 = (sin_value - 3.0 * sin_3value)
+   ψxx_up = ψxx + 0.25 * δ * ((r^2 + θx^2) * term1 + 4.0 * θx * θy * term2 - θy^2 * (cos_value - 9.0 * cos_3value)) / r
+   ψyy_up = ψyy + 0.25 * δ * ((r^2 + θy^2) * term1 - 4.0 * θx * θy * term2 - θx^2 * (cos_value - 9.0 * cos_3value)) / r
+   ψxy_up = ψxy + 0.25 * δ * (θx * θy * term1 - 2.0 * (θx^2 - θy^2) * term2 + θx * θy * (cos_value - 9.0 * cos_3value)) / r
+   return ψxx_up, ψyy_up, ψxy_up
 end
 
 
 """
     jacobian!(ψxx::T, ψyy::T, ψxy::T, θx::T, θy::T, δ::RV, ϕ::RV) where T <: ROA
+Calculate jacobian at given coordinates for "restricted" third order perturbations corresponding
+to SIS lens model and update the jacobian in place.
+
+# Arguments
+- `ψxx`: xx-component of jacobian at given coordinates
+- `ψyy`: yy-component of jacobian at given coordinates
+- `ψxy`: xy-component of jacobian at given coordinates
+- `θx`: x-coordinate(s) (in ``\\rm \\mathbf{arcseconds}``).
+- `θy`: y-coordinate(s) (in ``\\rm \\mathbf{arcseconds}``).
+- `δ::RV`: Amplitude of third order perturbations.
+- `ϕ::RV`: External Shear angle (in ``\\rm \\mathbf{degrees}``).
 """
 function jacobian!(ψxx::T, ψyy::T, ψxy::T, θx::T, θy::T, δ::RV, ϕ::RV) where T <: ROA
+   # Precompute angles
+   ϕ = deg2rad(ϕ)
+   cos_phi = cos(ϕ)
+   sin_phi = sin(ϕ)
    
+   ax1, ax2 = axes(θx, 1), axes(θx, 2)
+   @inbounds for j in ax2
+      @inbounds for i in ax1
+         # Get radial coordinate
+         dx = θx[i, j]
+         dy = θy[i, j]
+         r = sqrt(dx^2 + dy^2)
+
+         # cos(θ - ϕ) and sin(θ - ϕ)
+         cos_value = (dx * cos_phi + dy * sin_phi) / r
+         sin_value = (dy * cos_phi - dx * sin_phi) / r
+
+         # cos3(θ - ϕ) and sin3(θ - ϕ)
+         cos_3value = 4.0 * cos_value^3 - 3.0 * cos_value
+         sin_3value = 3.0 * sin_value - 4.0 * sin_value^3
+
+         # Jacobian
+         term1 = 3.0 * (cos_value - cos_3value)
+         term2 = (sin_value - 3.0 * sin_3value)
+         ψxx[i, j] = ψxx[i, j] + 0.25 * δ * ((r^2 + dx^2) * term1 + 4.0 * dx * dy * term2 - dy^2 * (cos_value - 9.0 * cos_3value)) / r
+         ψyy[i, j] = ψyy[i, j] + 0.25 * δ * ((r^2 + dy^2) * term1 - 4.0 * dx * dy * term2 - dx^2 * (cos_value - 9.0 * cos_3value)) / r
+         ψxy[i, j] = ψxy[i, j] + 0.25 * δ * (dx * dy * term1 - 2.0 * (dx^2 - dy^2) * term2 + dx * dy * (cos_value - 9.0 * cos_3value)) / r
+      end
+   end
 end
 
 end
--- test/test_Lenses/test_Lenses_ExternalEffects3.jl
+++ test/test_Lenses/test_Lenses_ExternalEffects3.jl
@@ -1,13 +1,22 @@
-#!!!!!!!!!!!!!! Testing AGAINST formulae !!!!!!!!!!!!!!
+#!!!!!!!!!!!!!! Testing AGAINST Glafic !!!!!!!!!!!!!!
+#! Keep in mind that coordinates are rotated by 90 degree in Glafic compared to LensFactory. Hence,
+#! 0° in LensFactory is equal to 90° in Glafic.
 @testset "ExternalEffects3 lens" begin
    delta = 0.2
    angle = 90.0
+   
    lens = Lenses.init_ExternalEffects3(delta=delta, angle=angle)
    pot1 = Lenses.get_potential(lens, xt1, yt1)
    dex1 = Lenses.get_deflection(lens, xt1, yt1)
-
+   jac1 = Lenses.get_jacobian(lens, xt1, yt1)
+   kappa =  0.5 * (jac1[1] + jac1[2])
+   gamma1 = 0.5 * (jac1[1] - jac1[2])
+   gamma2 = jac1[3]
    @test dex1[1] ≈ 0.4 atol=1e-15 rtol=1e-15
    @test dex1[2] ≈ 0.2 atol=1e-15 rtol=1e-15
+   @test kappa  ≈ 0.2  atol=1e-4 rtol=1e-4
+   @test gamma1 ≈ 0.2 atol=1e-4 rtol=1e-4
+   @test gamma2 ≈ 0.4 atol=1e-4 rtol=1e-4
 
 
 end
