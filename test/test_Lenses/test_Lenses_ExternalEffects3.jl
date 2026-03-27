593fa60d1aef784a5e4d77dafdc5c868c5815a07
akmeena766 <ashishmeena766@gmail.com>
2026-03-27 15:00:43 +0530

    version 0.0.6+dev

--- test/test_Lenses/test_Lenses_ExternalEffects3.jl
+++ test/test_Lenses/test_Lenses_ExternalEffects3.jl
@@ -14,9 +14,36 @@
    gamma2 = jac1[3]
    @test dex1[1] ≈ 0.4 atol=1e-15 rtol=1e-15
    @test dex1[2] ≈ 0.2 atol=1e-15 rtol=1e-15
-   @test kappa  ≈ 0.2  atol=1e-4 rtol=1e-4
-   @test gamma1 ≈ 0.2 atol=1e-4 rtol=1e-4
-   @test gamma2 ≈ 0.4 atol=1e-4 rtol=1e-4
+   @test kappa  ≈ 0.2  atol=1e-15 rtol=1e-15
+   @test gamma1 ≈ 0.2 atol=1e-15 rtol=1e-15
+   @test gamma2 ≈ 0.4 atol=1e-15 rtol=1e-15
+
+   pot2 = Lenses.get_potential(lens, xt2, yt2)
+   dex2 = Lenses.get_deflection(lens, xt2, yt2)
+   jac2 = Lenses.get_jacobian(lens, xt2, yt2)
+   @test dex2[1] ≈ 0.0 atol=1e-15 rtol=1e-15
+   @test dex2[2] ≈ 0.2 atol=1e-15 rtol=1e-15
+   @test jac2[1] ≈ 0.0 atol=1e-15 rtol=1e-15
+   @test jac2[2] ≈ 0.0 atol=1e-15 rtol=1e-15
+   @test jac2[3] ≈ 0.4 atol=1e-15 rtol=1e-15
+
+   potc = Lenses.get_potential(lens, [xt1, xt2], [yt1, yt2])
+   dexc = Lenses.get_deflection(lens, [xt1, xt2], [yt1, yt2])
+   jacc = Lenses.get_jacobian(lens, [xt1, xt2], [yt1, yt2])
+   @test potc[1] ≈ pot1 atol=1e-15 rtol=1e-15
+   @test potc[2] ≈ pot2 atol=1e-15 rtol=1e-15
+   
+   @test dexc[1][1] ≈ dex1[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][1] ≈ dex1[2] atol=1e-15 rtol=1e-15
+   @test dexc[1][2] ≈ dex2[1] atol=1e-15 rtol=1e-15
+   @test dexc[2][2] ≈ dex2[2] atol=1e-15 rtol=1e-15
+
+   @test jacc[1][1] ≈ jac1[1] atol=1e-15 rtol=1e-15
+   @test jacc[1][2] ≈ jac2[1] atol=1e-15 rtol=1e-15
+   @test jacc[2][1] ≈ jac1[2] atol=1e-15 rtol=1e-15
+   @test jacc[2][2] ≈ jac2[2] atol=1e-15 rtol=1e-15
+   @test jacc[3][1] ≈ jac1[3] atol=1e-15 rtol=1e-15
+   @test jacc[3][2] ≈ jac2[3] atol=1e-15 rtol=1e-15
 
 
 end
