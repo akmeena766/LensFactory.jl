5233e307f0594a0fa2491f7827f6bdcac89ab066
akmeena766 <ashishmeena766@gmail.com>
2026-05-25 09:44:24 +0530

    version 0.0.8+dev

--- src/SingularityMap/SingularityMap.jl
+++ src/SingularityMap/SingularityMap.jl
@@ -11,6 +11,7 @@ module SingularityMap
 # --------------------------------------------------------------------------------------------------
 using ..Constants
 using ..Lenses
+using ..LFUtils
 
 
 # --------------------------------------------------------------------------------------------------
@@ -30,6 +31,9 @@ include("./a4points.jl")
 include("./d4points.jl")
 
 
+# --------------------------------------------------------------------------------------------------
+# This is the common function that will be used by both from_jacobian and from_lens
+# --------------------------------------------------------------------------------------------------
 function _common(ddf::Array{Float64, 3}, θx::T, θy::T; adis::Float64=1.0, buffer::Int64=10) where T<:Matrix{Float64}
    # Dimensions
    nx, ny = size(θx)
@@ -82,8 +86,56 @@ function _common(ddf::Array{Float64, 3}, θx::T, θy::T; adis::Float64=1.0, buff
    nd4::Int64 = 0
    rd4 = zeros(Float64, round(Int64, nx * ny / 8), 2)
    nd4 = _d4points(nd4, rd4, ddf)
+
+   # -----------------------------------------------------------------------------------------------
+   # Remove points with a_crit > adis and covert positions from pixel to arcsecond
+   # -----------------------------------------------------------------------------------------------
+   na31_ref::Int64 = 0
+   ra31_ref = zeros(Float64, round(Int64, nx * ny / 8), 2)
+   for i in 1:na31
+      e1_val = PolygonOps.bilinear_interpolation(ra31[i, 1], ra31[i, 2], e1)
+      if adis * e1_val ≥ 1.0
+         na31_ref = na31_ref + 1
+
+         # Convert from pixel to arcsecond
+         θx_value = PolygonOps.bilinear_interpolation(ra31[i, 1], ra31[i, 2], θx)
+         θy_value = PolygonOps.bilinear_interpolation(ra31[i, 1], ra31[i, 2], θy)
+         ra31_ref[na31_ref, 1] = θx_value
+         ra31_ref[na31_ref, 2] = θy_value
+      end
+   end
+
+   na32_ref::Int64 = 0
+   ra32_ref = zeros(Float64, round(Int64, nx * ny / 8), 2)
+   for i in 1:na32
+      e2_val = PolygonOps.bilinear_interpolation(ra32[i, 1], ra32[i, 2], e2)
+      if adis * e2_val ≥ 1.0
+         na32_ref = na32_ref + 1
+
+         θx_value = PolygonOps.bilinear_interpolation(ra32[i, 1], ra32[i, 2], θx)
+         θy_value = PolygonOps.bilinear_interpolation(ra32[i, 1], ra32[i, 2], θy)
+         ra32_ref[na32_ref, 1] = θx_value
+         ra32_ref[na32_ref, 2] = θy_value
+      end
+   end
+
+
+   nd4_ref::Int64 = 0
+   rd4_ref = zeros(Float64, round(Int64, nx * ny / 8), 2)
+   for i in 1:nd4
+      e1_val = PolygonOps.bilinear_interpolation(rd4[i, 1], rd4[i, 2], e1)
+      e2_val = PolygonOps.bilinear_interpolation(rd4[i, 1], rd4[i, 2], e2)
+      if adis * e1_val ≥ 1.0 && adis * e2_val ≥ 1.0
+         nd4_ref = nd4_ref + 1
+
+         θx_value = PolygonOps.bilinear_interpolation(rd4[i, 1], rd4[i, 2], θx)
+         θy_value = PolygonOps.bilinear_interpolation(rd4[i, 1], rd4[i, 2], θy)
+         rd4_ref[nd4_ref, 1] = θx_value
+         rd4_ref[nd4_ref, 2] = θy_value
+      end
+   end
    
-   return ra31[1:na31, :], ra32[1:na32, :], rd4[1:nd4, :]
+   return ra31_ref[1:na31_ref, :], ra32_ref[1:na32_ref, :], rd4_ref[1:nd4_ref, :]
 end
 
 
@@ -134,11 +186,10 @@ function from_lens(lens::Lenses.AbstractLens, θx::T, θy::T;
    ddf[:, :, 2] = ψyy
    ddf[:, :, 3] = ψxy
 
-   
-
-
    # Pass jacobian map to from_jacobian function
-   return _common(ψxx, ψyy, ψxy, θx, θy; )
+   ra31, ra32, rd4 = _common(ddf, θx, θy; adis=adis, buffer=buffer)
+
+   return ra31, ra32, rd4
 end
 
 
